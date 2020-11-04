8f317e022b26112c497c7f6e50354a55c55e4bbd
alex-pfeifer <apfeif.12@gmail.com>
2020-11-04 07:55:23 -0800

    all done

--- index.js
+++ index.js
@@ -224,9 +224,12 @@ Using the annoyingSong function below do the following:
       "(number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+function annoyingSong(i){
+        while (i > 1)
+          return i + " bottles of soda on the wall, " + i + " bottles of soda, take one down pass it around " + (i - 1) + " bottles of soda on the wall"
+        }
+
+
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
